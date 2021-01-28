a2c361654205d73f7e05ff8e19803fcfcb9c89e3
BSharma <bsharma@digitalrealty.com>
2021-01-28 13:28:48 -0500

    Refactored file for cleanup

--- src/main/java/com/solstice/azure/springcloud/order/OrderController.java
+++ src/main/java/com/solstice/azure/springcloud/order/OrderController.java
@@ -1,5 +1,6 @@
 package com.solstice.azure.springcloud.order;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,41 +16,28 @@ import java.util.List;
 @RestController
 public class OrderController {
 
-    private InventoryServiceClient inventoryServiceClient;
+    @Value("${producer.endpoint}")
+    private String producerEndPoint;
 
     public OrderController(InventoryServiceClient inventoryServiceClient) {
-        this.inventoryServiceClient = inventoryServiceClient;
     }
-
-    /*@PostMapping("/order")
-    public ResponseEntity<String> submitOrder(@RequestBody List<Order> orders) {
-
-        List<Inventory> inventory = createInventoryRequest(orders);
-        inventoryServiceClient.updateInventory(inventory);
-        return new ResponseEntity<String>("Order Submitted Successfully",
-                HttpStatus.OK);
-    }*/
 
     @PostMapping("/order")
     public ResponseEntity<String> submitOrder(@RequestBody List<Order> orders) throws URISyntaxException {
         RestTemplate restTemplate = new RestTemplate();
-        final String baseUrl = "http://localhost:8090/inventory";
-        URI uri = new URI(baseUrl);
         List<Inventory> inventory = createInventoryRequest(orders);
-        ResponseEntity<String> result = restTemplate.postForEntity(uri, inventory, String.class);
-        return new ResponseEntity<String>("Order Submitted Successfully", HttpStatus.OK);
+        ResponseEntity<String> result = restTemplate.postForEntity(new URI(producerEndPoint), inventory, String.class);
+        return new ResponseEntity<>("Order Submitted Successfully: ", HttpStatus.OK);
     }
 
     private List<Inventory> createInventoryRequest(List<Order> orders) {
         List<Inventory> inventories = new ArrayList<>();
-        orders.forEach(order -> {
-            inventories.add(Inventory.builder()
-                    .id(order.getInventoryId())
-                    .productName(order.getProductName())
-                    .productCategory(order.getProductCategory())
-                    .quantity(order.getQuantity())
-                    .build());
-        });
+        orders.forEach(order -> inventories.add(Inventory.builder()
+                .id(order.getInventoryId())
+                .productName(order.getProductName())
+                .productCategory(order.getProductCategory())
+                .quantity(order.getQuantity())
+                .build()));
 
         return inventories;
     }
